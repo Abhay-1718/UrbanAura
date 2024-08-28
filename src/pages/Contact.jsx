5cc15946eb159d668793e92a0de7e5f5cef1d516
Abhay-1718 <abhaysingh80766@gmail.com>
2024-08-28 12:54:22 +0530

    message

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,8 +13,8 @@ const Contact = () => {
         <img  className="w-full md:max-w-[480px]" src={assets.contact_img} alt="" />
         <div className="flex flex-col justify-center items-start gap-6">
      <p className="font-semibold text-xl text-gray-600">Our Store</p>
-     <p className="text-gray-600">54709 Willims Station <br /> Suite 350,india usa </p>
-     <p className="text-gray-600">Tel: (415) 555-0132 <br />Email: admin@gmail.com</p>
+     <p className="text-gray-600">Mayur Vihar  <br /> Delhi, India </p>
+     <p className="text-gray-600">Tel:8076628*** <br />Email: abhaysingh80766@gmail.com</p>
      <p className="font-semibold text-xl text-gray-600">Carrers At Forever</p>
      <p className="text-gray-600">Learn More About Our Teams And Job Openings </p>
   <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">Explore Jobs </button>
